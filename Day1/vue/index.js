b79eddbfd328a8de940daf32517f398d9db24419
alexcsych <alexsejsi4@gmail.com>
2023-10-16 15:42:35 +0300

    Add vue3

--- Day1/vue/index.js
+++ Day1/vue/index.js
@@ -21,25 +21,47 @@ var quotesArray = [
   }
 ]
 
-new Vue({
-  el: '#app',
-  data: {
-    quotes: quotesArray,
-    isAuthor: '',
-    sortedQuotes: []
-  },
-  computed: {
-    filteredQuotes: function () {
-      return this.quotes.filter(e => {
-        return Boolean(this.isAuthor) == Boolean(e.source)
+const { createApp, ref, computed } = Vue
+createApp({
+  setup () {
+    const quotes = ref(quotesArray)
+    let isAuthor = ref('')
+    let sortedQuotes = ref([])
+
+    const filteredQuotes = computed(() => {
+      return [...quotes.value].filter(e => {
+        return Boolean(isAuthor.value) == Boolean(e.source)
       })
-    }
-  },
-  methods: {
-    sortBySource () {
-      this.sortedQuotes = this.quotes
+    })
+
+    const sortBySource = () => {
+      sortedQuotes.value = [...quotes.value]
         .slice()
         .sort((a, b) => (a.source > b.source ? 1 : -1))
     }
+    return { quotes, isAuthor, sortedQuotes, filteredQuotes, sortBySource }
   }
-})
+}).mount('#app')
+
+// new Vue({
+//   el: '#app',
+//   data: {
+//     quotes: quotesArray,
+//     isAuthor: '',
+//     sortedQuotes: []
+//   },
+//   computed: {
+//     filteredQuotes: function () {
+//       return this.quotes.filter(e => {
+//         return Boolean(this.isAuthor) == Boolean(e.source)
+//       })
+//     }
+//   },
+//   methods: {
+//     sortBySource () {
+//       this.sortedQuotes = this.quotes
+//         .slice()
+//         .sort((a, b) => (a.source > b.source ? 1 : -1))
+//     }
+//   }
+// })
